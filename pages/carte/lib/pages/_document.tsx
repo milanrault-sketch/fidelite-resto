23a3498c044b2480aa52b776c8e76f87b7568da0
milanrault-sketch <milan.rault@gmail.com>
2026-04-01 21:58:14 +0200

    Create _document.tsx

---
+++ pages/carte/lib/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Fidélité" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
